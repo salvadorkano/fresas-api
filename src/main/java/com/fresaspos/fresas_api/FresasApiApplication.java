9227f5816bd58e61ee197e25247e5fd63e3002e1
Rogerpervaz <ulisescodi@gmail.com>
2026-09-22 22:55:23 -0600

    first commit

---
+++ src/main/java/com/fresaspos/fresas_api/FresasApiApplication.java
@@ -0,0 +1,13 @@
+package com.fresaspos.fresas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FresasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FresasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fresaspos/fresas_api/FresasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fresaspos.fresas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FresasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
